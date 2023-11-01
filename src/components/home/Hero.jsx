0d36c81af140e6862bafa9819b5a8aab45ea8574
tolipov <ziyovuddin.developer@gmail.com>
2023-11-01 15:25:37 +0500

    home

--- src/components/home/Hero.jsx
+++ src/components/home/Hero.jsx
@@ -7,12 +7,12 @@ const Hero = () => {
 
             <div className="  overflow-y-hidden flex flex-col justify-between items-center h-[100vh] relative max-h-[800px]">
                 <header className="relative flex max-w-screen-xl flex-col overflow-hidden px-4 py-4 text-blue-900 md:mx-auto md:flex-row md:items-center h-[100px]">
-                    <a href="#" className="flex cursor-pointer items-center whitespace-nowrap text-2xl font-black text-[#00ffcb]">
+                    <Link to="/" className="flex cursor-pointer items-center whitespace-nowrap text-2xl font-black text-[#00ffcb]">
                         <span className="mr-2 text-4xl">
                             <svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="img" width="1em" height="1em" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24"><path fill="currentColor" d="M6.925 16.875Q5.2 16.225 4.1 14.713Q3 13.2 3 11.25q0-1.975.938-3.513Q4.875 6.2 6 5.15q1.125-1.05 2.062-1.6L9 3v2.475q0 .625.45 1.062q.45.438 1.075.438q.35 0 .65-.15q.3-.15.5-.425L12 6q.95.55 1.625 1.35t1.025 1.8l-1.675 1.675q-.05-.6-.287-1.175q-.238-.575-.638-1.05q-.35.2-.738.287q-.387.088-.787.088q-1.1 0-1.987-.612Q7.65 7.75 7.25 6.725q-.95.925-1.6 2.062Q5 9.925 5 11.25q0 .775.275 1.462q.275.688.75 1.213q.05-.5.287-.938q.238-.437.588-.787L9 10.1l2.15 2.1q.05.05.1.125t.1.125l-1.425 1.425q-.05-.075-.087-.125q-.038-.05-.088-.1L9 12.925l-.7.7q-.125.125-.212.287q-.088.163-.088.363q0 .3.175.537q.175.238.45.363ZM9 10.1Zm0 0ZM7.4 22L6 20.6L19.6 7L21 8.4L17.4 12H21v2h-5.6l-.5.5l1.5 1.5H21v2h-2.6l2.1 2.1l-1.4 1.4l-2.1-2.1V22h-2v-4.6l-1.5-1.5l-.5.5V22h-2v-3.6Z" /></svg>
                         </span>
-                        streamio
-                    </a>
+                        SBTUIT LIB
+                    </Link>
                     <input type="checkbox" className="peer hidden" id="navbar-open" />
                     <label className="absolute top-5 right-7 cursor-pointer md:hidden text-blue-600" htmlFor="navbar-open">
                         <span className="sr-only">Toggle Navigation</span>
@@ -50,12 +50,12 @@ const Hero = () => {
                                 <p className="text-base text-gray-400">Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa, sunt?</p>
                             </div>
                             <div className="mt-10 flex flex-col items-center md:flex-row">
-                                <a href="/" className="mb-3 inline-flex h-12 w-full items-center justify-center rounded bg-blue-700 px-6 font-medium tracking-wide text-white shadow-md transition md:mr-4 md:mb-0 md:w-auto focus:outline-none hover:bg-blue-800">Sinab ko'rish </a>
-                                <a href="/" aria-label="" className="group inline-flex items-center font-semibold text-white">Qo'llanma
+                                <Link  to="/" className="mb-3 inline-flex h-12 w-full items-center justify-center rounded bg-blue-700 px-6 font-medium tracking-wide text-white shadow-md transition md:mr-4 md:mb-0 md:w-auto focus:outline-none hover:bg-blue-800">Sinab ko'rish </Link>
+                                <Link to="/about" aria-label="" className="group inline-flex items-center font-semibold text-white">Qo'llanma
                                     <svg xmlns="http://www.w3.org/2000/svg" className="group-hover:translate-x-2 ml-4 h-6 w-6 transition-transform" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
                                         <path strokeLinecap="round" strokeLinejoin="round" d="M17 8l4 4m0 0l-4 4m4-4H3" />
                                     </svg>
-                                </a>
+                                </Link>
                             </div>
                         </div>
                         <div className="relative hidden lg:ml-32 lg:block lg:w-1/2">
