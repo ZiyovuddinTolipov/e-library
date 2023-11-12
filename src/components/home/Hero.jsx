7c3893f153a1f8c42bc8f47ab64720a9a13149eb
tolipov <ziyovuddin.developer@gmail.com>
2023-11-12 17:29:24 +0500

    hero

--- src/components/home/Hero.jsx
+++ src/components/home/Hero.jsx
@@ -7,40 +7,40 @@ const Hero = () => {
             <div className='max-w-[1400px] w-full mx-auto'>
             <nav className='flex justify-between items-center py-4'> {/*Navbar qismi */}
                 <div>
-                    <h3 className='font-semibold'>
+                    <h4 className='font-semibold'>
                         <span className='text-[#45B871]'>TUIT</span>-LIB
-                    </h3>
+                    </h4>
                 </div>
-                <ul className='flex items-center  gap-4'>
+                <ul className='flex items-center  gap-4 font-[500]'>
                     <li>HOME</li>
                     <li>All Courses</li>
                     <li>Pricing</li>
                     <li>Blog</li>
                 </ul>
-                <div>
+                <div className='flex items-center  gap-4 font-[500]'>
                     <button>Sign In</button>
-                    <button>Free Trial</button>
+                    <button className='bg-[#45B871] px-3 py-2 text-white rounded-md'>Free Trial</button>
                 </div>
             </nav>
-            <div>
-                <div>
-                    <h1>
+            <div className='flex py-5 max-h-[650px] h-full'>
+                <div className='w-1/2 px-24 flex flex-col justify-between '>
+                    <h1 className='text-5xl font-[700]'>
                         Anytime,anywhereDevelop your skillby Online
                     </h1>
-                    <p>
+                    <p className='text-xl'>
                         Online education is a form of education where students use their home 
                         computers through the internet. For many nontraditional students, among 
                         them all those who want to continue.
                     </p>
                     <div>
-                        <button>All Courses</button>
+                        <button className='bg-[#45B871] py-2 px-3 text-white  rounded-md'>All Courses</button>
                         <button>
                             <i>icon</i>
                             Play Video
                         </button>
                     </div>
                 </div>
-                <div>
+                <div className='w-1/2'>
                     <div></div>
                     <div></div>
                     <div></div>
